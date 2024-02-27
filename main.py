6de8fdaad6959fbf36a9d75c0f678cfbe14ea91d
Hans Achterbahn <hans.achterbahn@posteo.de>
2024-02-27 21:13:31 +0100

    experiments with reading csv and json files → looks like pandas works best

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-from tabulate import tabulate
+import csv
+import json
+import pandas
 import pandas as pd
-from csv_diff import load_csv, compare
+import datetime
+import numpy as np
+from tabulate import tabulate
 
 def tab(dataframe):
     print(tabulate(dataframe, headers='keys', tablefmt='github'))
     print("")
 
-def csv_diff_pandas():
-    df_new = pd.read_csv('data.csv', index_col=0)
-    df_old = pd.read_csv('data.old.csv', index_col=0)
-
-    tab(df_new)
-    tab(df_old)
-
-    comparison_df = pd.merge(df_old, df_new, indicator=True, how="right")
-    tab(comparison_df)
-
-    diff_df = comparison_df[comparison_df["_merge"] != "both"]
-    tab(diff_df)
-
-    tab(df_old.drop(df_new.index))
-
 # Use csv-diff
 # - looks quite òk -> easy to use and straight forward
-def csv_diff_csv_diff():
-    file_new = load_csv(open("data.csv"))
-    file_old = load_csv(open("data.old.csv"))
+def csv_read():
+    with open("data.csv", newline="") as csv_file_handler:
+        csv_file = csv.reader(csv_file_handler)
+        for row in csv_file:
+            print('\t '.join(row))
 
-    print(file_old)
+def csv_read_pandas():
+    df_new = pandas.read_csv("data.csv")
+    print(df_new["Zeitstempel"])
+    df_new["Zeitstempel"] = pd.to_datetime(df_new["Zeitstempel"], yearfirst=True, utc=True)
+    print(df_new["Zeitstempel"])
+    #df_new.query(df_new["Zeitstempel"] > np.datetime64("2022-01-01 00:00:00", "ns"))
+    print(np.datetime64("2022-01-01 00:00:00", "ns"))
 
-    difference = compare(file_old,file_new)
-    print(difference['added'][0]['Arbeitskreis'])
+def json_read():
+    with open("data.json") as new_data_file:
+        new_data = json.load(new_data_file)
+
+        print(json.dumps(new_data, sort_keys=True, indent=2))
 
 if __name__ == "__main__":
-    #csv_diff_pandas()
-    csv_diff_csv_diff()
+    #csv_read()
+    #json_read()
+    csv_read_pandas()
