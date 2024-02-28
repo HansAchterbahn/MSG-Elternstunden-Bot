0dc47f261478421f114242719a224faa0dd81f8f
Hans Achterbahn <hans.achterbahn@posteo.de>
2024-02-28 21:22:50 +0100

    add toml config file + clean up

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import pandas
 import pandas as pd
-import numpy as np
+import tomli
 
 
-def csv_read():
-    df_new = pandas.read_csv("data.csv")
+last_timestamp = "2023-01-01 00:00:00"
+def csv_read(file_path):
+    df_new = pd.read_csv(file_path)        # read csv file to dataframe
+    df_new["Zeitstempel"] = (
+        pd.to_datetime(df_new["Zeitstempel"], yearfirst=True, utc=True)) # convert timestamp strings to UTC+0 values
     print(df_new["Zeitstempel"])
-    df_new["Zeitstempel"] = pd.to_datetime(df_new["Zeitstempel"], yearfirst=True, utc=True)
-    print(df_new["Zeitstempel"])
-    print(df_new.loc[df_new["Zeitstempel"] > "2023-01-01 00:00:00"])
-    print(np.datetime64("2022-01-01 00:00:00", "ns"))
-
+    print(df_new.loc[df_new["Zeitstempel"] > last_timestamp])
 
 if __name__ == "__main__":
-    csv_read()
+    with open("config.toml", "rb") as f:
+        toml_dict = tomli.load(f)
+        
+    csv_read("data.csv")
